69cb106774db0a912f0400109ed7dd4bca22fa9a
Dmytro Kardava <kardava.d.g@fex.net>
2020-02-04 10:11:58 +0100

    add wroclaw test route

--- database/seeds/RoutesSeeder.php
+++ database/seeds/RoutesSeeder.php
@@ -11,6 +11,28 @@ class RoutesSeeder extends Seeder
      */
     public function run()
     {
+        $wroclaw = \App\Route::create([
+            'title' => 'Wroclaw',
+        ]);
+
+        $wroclaw->pois()->createMany([
+            [
+                'title' => 'Galeria Dominikańska',
+                'latitude' => '51.108315',
+                'longitude' => '17.040418',
+            ],
+            [
+                'title' => 'Arkady Wrocławskie',
+                'latitude' => '51.099514',
+                'longitude' => '17.029194',
+            ],
+            [
+                'title' => 'Wroclavia',
+                'latitude' => '51.097223',
+                'longitude' => '17.033422',
+            ],
+        ]);
+
         factory(\App\Route::class, 100)->create()->each(function (\App\Route $route) {
             $route->pois()->saveMany(factory(\App\Poi::class, rand(3, 7))->create());
         });
